5b5a5abc51a4ee74f8e57c520e6ff8b29c490b89
disto-stack <juan.ocampo.2501@gmail.com>
2022-07-06 04:58:24 -0500

    implement mobile menu with javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-})
+  const mobileMenuButton = document.querySelector('#mobile-menu');
+  const mobileNavigation = document.querySelector('#app-navigation');
+
+  mobileMenuButton.addEventListener('click', () => {
+    mobileMenuButton.classList.toggle('active-mobile-menu');
+    mobileNavigation.classList.toggle('hidden-menu');
+  });
+});
